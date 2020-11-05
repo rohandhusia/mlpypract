2047a21e9f63769949cf2d9e832f9671190cea08
Rohan Dhusia <rohan.dhusia@gmail.com>
2020-11-05 21:15:47 +0530

    iris data set visualization

--- iris.py
+++ iris.py
@@ -3,7 +3,19 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 import sys
-print(np.__version__)
-print(pd.__version__)
-print(sb.__version__)
-print(sys.version)
+
+# print(np.__version__)
+# print(pd.__version__)
+# print(sb.__version__)
+# print(sys.version)
+
+# reading data from local
+# df = pd.read_csv("data/iris.csv")
+
+# reading data using seaborn
+iris = sb.load_dataset('iris')
+print(iris.head())
+print(iris.describe())
+print(iris.info())
+print(iris.groupby('species').size())
+
