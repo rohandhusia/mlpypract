6a74b0aa9f764009e6fdb697cb66b670cca9f57a
Rohan Dhusia <rohan.dhusia@gmail.com>
2020-11-05 20:26:11 +0530

    iris data set visualization

---
+++ iris.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import sys
+print(np.__version__)
+print(pd.__version__)
+print(sb.__version__)
+print(sys.version)
