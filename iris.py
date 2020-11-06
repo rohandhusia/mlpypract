dc91b13f41514b3ae8594a5cf8ef20640b0b75f8
Rohan Dhusia <rohan.dhusia@gmail.com>
2020-11-06 08:20:02 +0530

    iris data set visualization

--- iris.py
+++ iris.py
@@ -4,6 +4,8 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 import sys
 
+sb.set(color_codes=True)
+
 # print(np.__version__)
 # print(pd.__version__)
 # print(sb.__version__)
@@ -19,3 +21,22 @@ print(iris.describe())
 print(iris.info())
 print(iris.groupby('species').size())
 
+# visualization
+sb.pairplot(iris, hue='species')
+plt.show()
+iris.hist()
+plt.show()
+plt.figure()
+plt.subplot(2, 2, 1)
+sb.violinplot(x='species', y='sepal_length', data=iris)
+plt.subplot(2, 2, 2)
+sb.violinplot(x='species', y='sepal_width', data=iris)
+plt.subplot(2, 2, 3)
+sb.violinplot(x='species', y='petal_length', data=iris)
+plt.subplot(2, 2, 4)
+sb.violinplot(x='species', y='petal_width', data=iris)
+plt.show()
+iris.boxplot(by='species')
+plt.show()
+pd.plotting.scatter_matrix(iris)
+plt.show()
