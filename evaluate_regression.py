2e620aab2f5ade5a1014a6250f0a31431bde4d88
Rohan Dhusia <rohan.dhusia@gmail.com>
2020-11-20 20:50:35 +0530

    linear & robust regression

--- evaluate_regression.py
+++ evaluate_regression.py
@@ -19,28 +19,30 @@ y = df['MEDV'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+print("LINEAR")
 lr = LinearRegression()
 lr.fit(X_train, y_train)
 
-y_train_pred = lr.predict(X_train)
-y_test_pred = lr.predict(X_test)
+y_train_pred_lr = lr.predict(X_train)
+y_test_pred_lr = lr.predict(X_test)
 
 # Method 1: Residual analysis
 sb.set(style='darkgrid', context='notebook')
 plt.figure('Residual analysis')
-plt.scatter(y_train_pred, y_train_pred - y_train, c='blue', marker='o', label='training data')
-plt.scatter(y_test_pred, y_test_pred - y_test, c='orange', marker='*', label='test data')
-plt.xlabel('Predited Values')
+plt.scatter(y_train_pred_lr, y_train_pred_lr - y_train, c='blue', marker='o', label='training data')
+plt.scatter(y_test_pred_lr, y_test_pred_lr - y_test, c='orange', marker='*', label='test data')
+plt.xlabel('Predicted Values')
 plt.ylabel('Residuals')
 plt.legend(loc='upper left')
 plt.show()
+plt.close()
 
 # Method 2: Mean Squared Error
 print("Mean Squared Error")
-print("training data: ", mean_squared_error(y_train, y_train_pred))
-print("test data: ", mean_squared_error(y_test, y_test_pred))
+print("training data: ", mean_squared_error(y_train, y_train_pred_lr))
+print("test data: ", mean_squared_error(y_test, y_test_pred_lr))
 
 # Method 3: Coefficient of Determination
 print("Coefficient of Determination")
-print("training data: ", r2_score(y_train, y_train_pred))
-print("test data: ", r2_score(y_test, y_test_pred))
+print("training data: ", r2_score(y_train, y_train_pred_lr))
+print("test data: ", r2_score(y_test, y_test_pred_lr))
